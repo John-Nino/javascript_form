2c9fa60b64041827af8b2d6cf1a31ab90a21431b
John-Nino <jnquinal23-0384@cca.edu.ph>
2025-02-17 17:52:25 +0800

    javascript_form

---
+++ index.php
@@ -0,0 +1,19 @@
+<HTML>
+	<head>
+		<title>
+		Javascript Form
+		</title>
+	</head>
+	<body>
+		<h1> Student Record</h1> 
+		<table id="student_table">
+		</table>
+		
+		<script>
+			const studentTable = document.getElementById
+			("student_table");
+			
+			console.log(studentTable);
+		</script>
+	</body>
+</HTML>
